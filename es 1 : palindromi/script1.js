70146b0ed8e0bc3e607975e50905a97c72be8f54
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2023-11-27 13:39:41 +0100

    add 1°function parola

--- es 1 : palindromi/script1.js
+++ es 1 : palindromi/script1.js
@@ -16,4 +16,39 @@
         // aggiorna il risultato nell html
 
 
+var listaParole=[];
+// 1°
+function parola(){
+    // ottieni la parola dall'input
+    var newParola = document.getElementById('text').value;
+
+    // -verifica che l'input e vuoto 
+    if (newParola.trim() !==''){
+        addParola(newParola);
+
+        // verifca tramite la funzione se è palindromo 
+        var risultato= palindromo(newParola);
+        aggiornaRisultato(risultato);
+
+        // pulizia del campo input 
+        document.getElementById('text').value='';
+    }else{
+        // non accetta parole vuote
+        alert('inserisci una parola vuota');
+    }
+
+}
+// 2°
+function addParola(newParola){
+   
+}
+// 3°
+function palindromo(){
+
+}
+// 4°
+function aggiornaRisultato(){
+
+}
+
 
