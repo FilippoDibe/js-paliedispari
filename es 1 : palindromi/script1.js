4e1456dbdc9e0383d9009a1ebe73df0823bd479c
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2023-11-27 13:26:40 +0100

    fix schema concettuale

---
+++ es 1 : palindromi/script1.js
@@ -0,0 +1,19 @@
+// form con un imput dove inserisci una parola con un bottone invia
+//  1° funzione (parola) che fa : 
+        // -ottiene la parola dall'input
+        // -verifica che l'input e vuoto 
+        // -inserisce la parola nell'array
+        // -verfica se la parola e palindroma
+        // pulisce l'input
+        // inserisce la parola nell'array
+// 2° funzione (addParola)
+        //  -inserisce la parola nell'array
+// 3° funzione (palindromo)
+        // -confronta la parola e la sua conversione
+        // true:se è palindromo
+        // false:se non lo è
+// 4° funzione (aggiurnaRisultato)
+        // aggiorna il risultato nell html
+
+
+
