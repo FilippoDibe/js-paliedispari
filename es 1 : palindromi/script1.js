b9f54352f0115971219019d642af0976141484fa
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2023-11-27 14:55:28 +0100

    fix bug and check

--- es 1 : palindromi/script1.js
+++ es 1 : palindromi/script1.js
@@ -1,5 +1,5 @@
-// form con un imput dove inserisci una parola con un bottone invia
-//  1° funzione (parola) che fa : 
+// form con un imput dove inserisci una newItem con un bottone invia
+//  1° funzione (newItem) che fa : 
         // -ottiene la parola dall'input
         // -verifica che l'input e vuoto 
         // -inserisce la parola nell'array
@@ -14,41 +14,60 @@
         // false:se non lo è
 // 4° funzione (aggiurnaRisultato)
         // aggiorna il risultato nell html
+            // creando dei li da mettere in una lista che gia era nel html
+            // e nei li inserire il risultato se e palindormo o no 
 
 
-var listaParole=[];
-// 1°
-function parola(){
-    // ottieni la parola dall'input
-    var newParola = document.getElementById('text').value;
 
-    // -verifica che l'input e vuoto 
-    if (newParola.trim() !==''){
-        addParola(newParola);
 
-        // verifca tramite la funzione se è palindromo 
-        var risultato= palindromo(newParola);
-        aggiornaRisultato(risultato);
 
-        // pulizia del campo input 
-        document.getElementById('text').value='';
-    }else{
-        // non accetta parole vuote
-        alert('inserisci una parola vuota');
+
+var listaParole = [];
+
+// Riferimento all'elemento UL dove verrà stampata la lista
+var stampaLista = document.getElementById('lista-es-1');
+
+// Funzione che viene chiamata al click del bottone "Aggiungi"
+function aggiungiItem() {
+    // Ottenimento del valore dell'input
+    var newItem = document.getElementById('text').value;
+    // Verifica se l'input non è vuoto prima di aggiungere l'elemento alla lista
+    if (newItem.trim() !== '') {
+        // Aggiunta del nuovo elemento all'array
+        listaParole.push(newItem);
+        // Chiamata alla funzione per aggiornare la lista
+        refreshLista();
+        // Pulizia del campo di input
+        document.getElementById('text').value = '';
+        var risultato= palindromo(newItem);
+        refreshLista(risultato);
+        
+    } else {
+        // Avviso se l'utente cerca di aggiungere un elemento vuoto
+        alert('Inserisci un elemento valido');
     }
-
 }
-// 2°
-function addParola(newParola){
-   listaParole.push(newParola);
+function palindromo(newItem){
+    var parolaInvertita = newItem.split('').reverse().join('');
+    return newItem===parolaInvertita;
 }
-// 3°
-function palindromo(){
+// Funzione per aggiornare la lista
+function refreshLista() {
+    var listaAggiornata = document.getElementById('lista-es-1');
+    
+    listaAggiornata.innerHTML = '';
 
+    // Inizializzazione dell'indice per il ciclo while
+    var i = 0;
+
+    // Ciclo while per aggiornare la lista con gli elementi dell'array
+    while (i < listaParole.length) {
+        // Creazione di un nuovo elemento LI
+        var listItem = document.createElement('li');
+        listItem.textContent = listaParole[i] + ':' + (palindromo(listaParole[i])? 'palindromo': 'non palindromo');
+        listaAggiornata.appendChild(listItem);
+
+        i++;
+    }
 }
-// 4°
-function aggiornaRisultato(){
-
-}
-
 
